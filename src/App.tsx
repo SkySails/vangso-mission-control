dfa97c21d546d6248935b92cc5a92367b879dc16
Malte Hallström <malte.hallstrom@gmail.com>
2021-04-14 16:28:19 +0200

    feat: add default location and WIP point

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { Cesium3DTileset, Viewer } from "resium";
-import { createOsmBuildings, createWorldTerrain, IonResource } from "cesium";
+import { Camera, CameraFlyTo, Cesium3DTileset, Entity, Viewer } from "resium";
+import { createWorldTerrain, IonResource, Cartesian3 } from "cesium";
+
+const DEFAULT_LOCATION = Cartesian3.fromDegrees(13.345236, 57.98167, 30000);
+const AIRPLANE_LOCATION = Cartesian3.fromDegrees(13.345236, 57.98167, 0);
+const pointGraphics = { pixelSize: 10 };
+
 const App = () => (
   <Viewer full terrainProvider={createWorldTerrain()}>
     <Cesium3DTileset url={IonResource.fromAssetId(96188)} />
+    <Camera defaultZoomAmount={0} />
+    <CameraFlyTo duration={0} destination={DEFAULT_LOCATION} />
+    <Entity position={AIRPLANE_LOCATION} point={pointGraphics} />
   </Viewer>
 );
 export default App;

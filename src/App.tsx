f11a03c342fca62e11214b75938ff5aabb62e373
Malte Hallström <malte.hallstrom@gmail.com>
2021-04-14 20:52:01 +0200

    feat: add glider model

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
-import React from "react";
-import { Camera, CameraFlyTo, Cesium3DTileset, Entity, Viewer } from "resium";
-import { createWorldTerrain, IonResource, Cartesian3 } from "cesium";
+import React, { useEffect } from "react";
+import {
+  Camera,
+  CameraFlyTo,
+  Cesium3DTileset,
+  Entity,
+  Viewer,
+  Model,
+} from "resium";
+import {
+  createWorldTerrain,
+  IonResource,
+  Cartesian3,
+  Transforms,
+} from "cesium";
+import { getPositionReports } from "./lib/ogn";
 
-const DEFAULT_LOCATION = Cartesian3.fromDegrees(13.345236, 57.98167, 30000);
+const DEFAULT_LOCATION = Cartesian3.fromDegrees(17.215934, 59.10078, 200);
 const AIRPLANE_LOCATION = Cartesian3.fromDegrees(13.345236, 57.98167, 0);
 const pointGraphics = { pixelSize: 10 };
 
-const App = () => (
-  <Viewer full terrainProvider={createWorldTerrain()}>
-    <Cesium3DTileset url={IonResource.fromAssetId(96188)} />
-    <Camera defaultZoomAmount={0} />
-    <CameraFlyTo duration={0} destination={DEFAULT_LOCATION} />
-    <Entity position={AIRPLANE_LOCATION} point={pointGraphics} />
-  </Viewer>
-);
+const origin = Cartesian3.fromDegrees(17.215934, 59.10078, 43.4);
+const modelMatrix = Transforms.eastNorthUpToFixedFrame(origin);
+
+const App = () => {
+  useEffect(() => {
+    // getPositionReports().then((data) => console.log(data));
+  }, []);
+
+  return (
+    <Viewer
+      full
+      terrainProvider={createWorldTerrain()}
+      animation={false}
+      timeline={false}
+    >
+      <Cesium3DTileset url={IonResource.fromAssetId(96188)} />
+      <CameraFlyTo duration={0} destination={DEFAULT_LOCATION} />
+      <Model
+        url={"assets/glider.glb"}
+        modelMatrix={modelMatrix}
+        minimumPixelSize={128}
+        scale={0.001}
+      />
+    </Viewer>
+  );
+};
 export default App;
